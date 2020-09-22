17f6d125fe45735240f916c8496290c068e2653a
Rhiannon McGlone <rhm9643@utulsa.edu>
2020-09-22 11:14:48 -0500

    Assignment 2 Ready (9/22/20)

---
+++ child_script.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+#child script
+
+	cut -f 1,2,4 BTS_data.txt > columns.txt
+
+#create a file containing only columns for ID, year, and trappability
+
+        head -n1 columns.txt > header
+
+#create a file containing the header
+
+        grep 13BTS0[1-8] columns.txt > snakes.txt
+
+#create a file containing only snakes 13BTS010-13BTS089
+
+        sort -k3rg snakes.txt > sorted_snakes.txt
+
+#reverse sort the table by trappability
+
+        cat header sorted_snakes.txt > assn2.gz
+
+#combine files to create final table with the header
+
+	cat assn2.gz
+
+#write the contents of the table to the terminal
+
+rm columns.txt header snakes.txt sorted_snakes.txt
+
+#remove intermediate files
---
+++ mcglone_assn2.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#parent script
+
+touch child_script.sh
+
+#create a child script
+
+ ./child_script.sh
+
+#execute the child script to the terminal
+
+
